25b595d310f1fe935960b4a295c7481664958d59
Peachess <annasamokaeva2020@gmail.com>
2022-12-10 01:05:17 +0200

    feat: add popup for the reviews in testimonials section

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,18 +1,20 @@
 "use strict";
 
-alert(
-  "Please be kind and give me time till Friday to finish the project. Thank you!"
-);
+// alert(
+//   "Please be kind and give me time till Friday to finish the project. Thank you!"
+// );
+
+// show and hide burger menu
 
 const burgerMenu = document.querySelector(".nav__burger");
 const navMenu = document.querySelector(".nav__menu");
 const hero = document.querySelector(".hero");
-const shadow = document.querySelector("#shadow");
+const overlay = document.querySelector(".overlay");
 
 burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   navMenu.classList.toggle("active");
-  shadow.classList.toggle("active");
+  overlay.classList.toggle("active");
 });
 
 window.addEventListener("click", (e) => {
@@ -23,6 +25,24 @@ window.addEventListener("click", (e) => {
   ) {
     burgerMenu.classList.toggle("active");
     navMenu.classList.toggle("active");
-    shadow.classList.toggle("active");
+    overlay.classList.toggle("active");
   }
 });
+
+// show and hide pop up in testimonials
+const reviews = document.querySelectorAll(".testimonials__card");
+const popUps = document.querySelector(".testimonials__container");
+
+for (let i = 0; i < reviews.length; i++) {
+  reviews[i].addEventListener("click", () => {
+    const popUpWindow = document.createElement("div");
+    popUpWindow.className = "popup";
+    popUps.append(popUpWindow);
+    popUpWindow.innerHTML = reviews[i].innerHTML;
+    overlay.classList.toggle("active");
+    overlay.addEventListener("click", () => {
+      popUpWindow.remove();
+      overlay.classList.toggle("active");
+    });
+  });
+}
